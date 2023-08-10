23f813f837ca0a0c01ac70c1191601886fa7d62f
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-08-10 13:52:23 +0200

    added example button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h1>Vite + React + shadcn Template</h1>
+      <main className="h-screen flex flex-col items-center justify-center gap-5">
+        <h1>Vite + React + shadcn Template</h1>
+        <div>
+          <Button>I'm a awesome shadcn button!</Button>
+        </div>
+      </main>
     </>
   );
 }
